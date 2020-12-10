6ac1488ab52f678272aa10dd1a2347772d41259e
Pedro Dominguez Aguilera <dominguezaguilera15@gmail.com>
2020-12-10 17:36:24 +0100

    Refactor test bar-info.component

--- admin/src/pods/bar-info/bar-info.component.spec.tsx
+++ admin/src/pods/bar-info/bar-info.component.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen, wait, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BarInfoComponent, Props } from './bar-info.component';
 
